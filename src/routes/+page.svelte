87dc37dc1c168d455bbc0bd3069d7f411d1e9011
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-07 12:50:58 +0000

    home button --> should be on template though

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,57 @@
 <script lang="ts">
-import type { PageData } from './$types'
+	import type { PageData } from './$types';
 
-export let data: PageData
-let tracks = data.tracks
-let timer: NodeJS.Timeout;
-let searchTerm = "";
+	export let data: PageData;
+	let { tracks } = data;
+	let timer: NodeJS.Timeout;
+	let searchTerm = '';
 
-function fetchTracks() {
-    fetch(`/api/searchTracks?searchTerm=${searchTerm}`)
-    .then((res) => res.json())
-    .then((data) => {
-        tracks = data
-    })
-}
+	function fetchTracks() {
+		fetch(`/api/searchTracks?searchTerm=${searchTerm}`)
+			.then((res) => res.json())
+			.then((data) => {
+				tracks = data;
+			});
+	}
 
-function handleSearch(e: Event) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        const target = e.target as HTMLInputElement;
-        searchTerm = target.value;
-        fetchTracks()
-    }, 300)
-    
-}
-
+	function handleSearch(e: Event) {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			const target = e.target as HTMLInputElement;
+			searchTerm = target.value;
+			fetchTracks();
+		}, 300);
+	}
 </script>
 
 <div class="px-4">
-<h1 class="is-size-1 md-5">Tracks</h1>
+	<h1 class="is-size-1 md-5">Tracks</h1>
 
-<input 
-    type="search" 
-    placeholder="Search..." 
-    class="input mb-5" 
-    style="max-width:80ch" 
-    value={searchTerm} 
-    on:input={handleSearch}/>
+	<input
+		type="search"
+		placeholder="Search..."
+		class="input mb-5"
+		style="max-width:80ch"
+		value={searchTerm}
+		on:input={handleSearch}
+	/>
 
-<table class="table">
-    <thead>
-        <th>Track</th>
-        <th>Artist</th>
-        <th>Album</th>
-        <th>Genre</th>
-    </thead>
-    <tbody>
-        {#each tracks as track}
-        <tr>
-        <td>{track.trackName}</td>
-        <td>{track.artistName}</td>
-        <td><a href= {`/album/${track.albumId}`}> {track.albumTitle}</a> </td>
-        <td>{track.genre}</td>
-    </tr>
-        {/each}
-    </tbody>
-</table>
+	<table class="table">
+		<thead>
+			<th>Track</th>
+			<th>Artist</th>
+			<th>Album</th>
+			<th>Genre</th>
+		</thead>
+		<tbody>
+			{#each tracks as track}
+				<tr>
+					<td>{track.trackName}</td>
+					<td>{track.artistName}</td>
+					<td><a href={`/album/${track.albumId}`}> {track.albumTitle}</a> </td>
+					<td>{track.genre}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
--- src/routes/album/[albumId]/+page.svelte
+++ src/routes/album/[albumId]/+page.svelte
@@ -1,40 +1,45 @@
 <script lang="ts">
-	import type { PageData } from './$types'
-    export let data: PageData
-    </script>
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
 
 <div class="px-4">
-<h1 class="is-size-1">{data.album.albumTitle}</h1>
-<p class="is-size-4">By {data.album.artistName}</p>
-<table class="table mt-6">
-    <thead>
-        <tr>
-        <th>#</th>
-        <th>Track</th>
-        <th>Duration</th>
-    </tr>
-    </thead>
-    <tbody>
-        {#each data.tracks as track, i }
-        <tr>
-            <td>{i+1}</td>
-            <td>{track.trackName}</td>
-            <td>{Math.floor(track.trackMs / 1000)} s</td>
-        </tr>            
-        {/each}
-    </tbody>
-</table>
+	<h1 class="is-size-1">{data.album.albumTitle}</h1>
+	<div style="display:flex">
+		<p class="is-size-4 mr-6">By {data.album.artistName}</p>
+		<a class="button is-primary" href="/">Home</a>
+	</div>
+	<table class="table mt-6">
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Track</th>
+				<th>Duration</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.tracks as track, i}
+				<tr>
+					<td>{i + 1}</td>
+					<td>{track.trackName}</td>
+					<td>{Math.floor(track.trackMs / 1000)} s</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 
-<h2 class="is-size-3 mb-4 mt-6">Update Album Title</h2>
-<form method ="POST">
-    <input 
-        type="text"
-        class="input"
-        name="albumTitle"
-        value={data.album.albumTitle}
-        style="max-width: 50ch;"
-        />
-<input type="hidden" name="albumId" value={data.album.albumId} >
-        <button class="button is-primary" type="submit" formaction="?/updateAlbumTitle">Update Album Title</button>
-</form>
+	<h2 class="is-size-3 mb-4 mt-6">Update Album Title</h2>
+	<form method="POST">
+		<input
+			type="text"
+			class="input"
+			name="albumTitle"
+			value={data.album.albumTitle}
+			style="max-width: 50ch;"
+		/>
+		<input type="hidden" name="albumId" value={data.album.albumId} />
+		<button class="button is-primary" type="submit" formaction="?/updateAlbumTitle"
+			>Update Album Title</button
+		>
+	</form>
 </div>
